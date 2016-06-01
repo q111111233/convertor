0ac9e0d93e73cdd67006b16b238effecd2315d16
Alex Jacks and Bo Zhao <bo@>
2016-06-01 13:07:08 -0700

    both forms work

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 //Simple unit conversion code//
 //functions to be used by a cooking app that convert between units. For example, a function that converts gallons to liters. Create a project for this like you did for the calculator, so that you can return and build a front-end for this later//
 
+$(document).ready(function() {
+  $("form#convert-vol").submit(function(event){
+		var num1 = $("input#volume").val();
+		$(".liters").text(Gallon2Liter(num1));
+		event.preventDefault();
+
+	});
+	$("form#convert-length").submit(function(event){
+		var num2 = $("input#length").val();
+		$(".meters").text(Foot2Meter(num2));
+		event.preventDefault();
+
+	});
+});
+
 var Gallon2Liter = function(num1) {
+debugger;
 	return (num1 * 3.78541);
 };
 
@@ -10,8 +26,5 @@ var Foot2Meter = function(num2) {
 };
 
 
-var num1 = parseInt(prompt("Gimme a number of Gallons:"));
-var num2 = parseInt(prompt("Gimme a number of Feet:"));
-
-alert(Gallon2Liter(num1));
-alert(Foot2Meter(num2));
+// (Gallon2Liter(num1));
+// (Foot2Meter(num2));
